f789e05071764014907fbd2113c5b687bcd1c66a
Carter Cobb <carter.cobb72@gmail.com>
2021-01-15 17:03:36 -0700

    request password reset route

--- Routes/user.js
+++ Routes/user.js
@@ -7,6 +7,7 @@ import { authenticateUser } from "../Helpers/auth.js";
 import { sendError, sendDatabaseError } from "../Helpers/exception-handlers.js";
 import eHATEOAS from "../Helpers/eHATEOAS.js";
 import { generateLinks } from "../Helpers/functions.js";
+import { sendEmail } from "../Helpers/email-client.js";
 const { ALL, DELETE, PATCH, POST, PUT } = eHATEOAS;
 
 const routes = [
@@ -99,7 +100,11 @@ const routes = [
         );
       }
       const keys = Object.keys(req.body);
-      if (!keys.includes("name") || !keys.includes("address")) {
+      if (
+        !keys.includes("password") ||
+        !keys.includes("name") ||
+        !keys.includes("address")
+      ) {
         return res.status(400).json({ error: "Please provide all fields" });
       }
       delete req.body.role;
@@ -185,6 +190,54 @@ const routes = [
           if (err.code && err.code === 11000) {
             return sendError(400, req, res, err, "BAD_REQUEST");
           }
+          return sendError(500, req, res, err);
+        }
+      } else {
+        return sendDatabaseError(req, res);
+      }
+    },
+  },
+  {
+    url: "/api/user/:id/request-new-pass",
+    type: eRequestType.POST,
+    handler: authenticateUser,
+    callback: async (req, res) => {
+      if (req.user.role !== "ADMIN" && req.user._id != req.params.id) {
+        return sendError(
+          403,
+          req,
+          res,
+          { message: "Cannot request passwod reset for other users." },
+          "FORBIDDEN"
+        );
+      }
+      if (connection.readyState === 1) {
+        try {
+          const new_pass = Date.now().toString(16);
+          const send = await sendEmail(
+            req.user.email,
+            "🔒 Password Reset - Lawn Care API",
+            new_pass
+          );
+          if (send.error) {
+            return sendError(500, req, res, send.error);
+          }
+          await User.updateOne(
+            { _id: req.params.id },
+            {
+              $set: {
+                password: bcrypt.hashSync(new_pass, bcrypt.genSaltSync(10)),
+              },
+            },
+            { upsert: false }
+          );
+          return res.status(200).json({
+            _embedded: {
+              message: "Check your email for your new temporary password.",
+            },
+            _links: {},
+          });
+        } catch (err) {
           return sendError(500, req, res, err);
         }
       } else {
