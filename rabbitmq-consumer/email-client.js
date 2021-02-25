1501b8a7137fc04e6abaa52aebc664291ee91ce1
Carter Cobb <carter.cobb72@gmail.com>
2021-02-24 20:39:40 -0700

    email client for consumer

---
+++ rabbitmq-consumer/email-client.js
@@ -0,0 +1,37 @@
+import mail from "nodemailer";
+const { createTransport } = mail;
+import { EMAIL_PASS, EMAIL_USERNAME, FROM_EMAIL } from "./KEYS.js";
+
+/**
+ * Sends an email
+ * @param {String} to email to send content to
+ * @param {String} subject email subject
+ * @param {String} text email body/text
+ * @param {String} html email html to send
+ *
+ * @return {Object} {message: "message"} on success
+ * @return {Object} {error: "description"} on fail
+ */
+export const sendEmail = async (to, subject, html) => {
+  var transporter = createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: `${EMAIL_USERNAME}`,
+      pass: `${EMAIL_PASS}`,
+    },
+  });
+  await transporter
+    .sendMail({
+      from: `Lawn Care API <${FROM_EMAIL}>`,
+      replyTo: `${FROM_EMAIL}`,
+      to,
+      subject,
+      html,
+    })
+    .catch((err) => {
+      return { error: err.message };
+    });
+  return { message: "Email Sent" };
+};
