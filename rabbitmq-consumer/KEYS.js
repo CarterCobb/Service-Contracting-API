1a4d2b3bfb71861073f499bd565f2a6235522176
Carter Cobb <carter.cobb72@gmail.com>
2021-02-24 20:39:58 -0700

    Added email keys

--- rabbitmq-consumer/KEYS.js
+++ rabbitmq-consumer/KEYS.js
@@ -9,3 +9,8 @@ export const RABBITMQ_ROUTING_KEY = process.env.RABBITMQ_ROUTING_KEY;
 export const RABBITMQ_HOST = process.env.RABBITMQ_HOST;
 export const RABBITMQ_USERNAME = process.env.RABBITMQ_USERNAME;
 export const RABBITMQ_PASSWORD = process.env.RABBITMQ_PASSWORD;
+
+//Email
+export const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
+export const EMAIL_PASS = process.env.EMAIL_PASS;
+export const FROM_EMAIL = process.env.FROM_EMAIL;
